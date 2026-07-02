929e5663cd43c89eccc4a28f03bba6577805d44c
siddhardh-codemonk <siddhardhgopi@gmail.com>
2026-07-02 11:46:33 +0530

    Initial commit - College Event Website

---
+++ college_event_management_website/src/main/java/com/college/college_event_management_website/CollegeEventManagementWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.college.college_event_management_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeEventManagementWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeEventManagementWebsiteApplication.class, args);
+	}
+
+}
---
+++ college_event_management_website/src/test/java/com/college/college_event_management_website/CollegeEventManagementWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.college_event_management_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeEventManagementWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
